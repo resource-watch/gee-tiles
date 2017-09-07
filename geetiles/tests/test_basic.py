594b2e7325bacae1e0e33fe6e599991617b14ba5
rrequero <rrequero@gmail.com>
2017-09-07 15:43:45 +0200

    fix tests

--- geetiles/tests/test_basic.py
+++ geetiles/tests/test_basic.py
@@ -1,23 +1,8 @@
 import unittest
 import json
-from geetiles import app
 import requests
-from httmock import all_requests, response, HTTMock
-
-
-@all_requests
-def response_content(url, request):
-    headers = {'content-type': 'application/json'}
-    content = {'data': 'any value'}
-    return response(200, content, headers, None, 5, request)
 
 class BasicTest(unittest.TestCase):
-
-    def setUp(self):
-        app.testing = True
-        app.config['TESTING'] = True
-        app.config['DEBUG'] = False
-        self.app = app.test_client()
 
     def tearDown(self):
         pass
