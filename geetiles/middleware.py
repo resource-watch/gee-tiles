6f8c985763c69d79a12435689fe2ceda5045e92c
Tiago Garcia <tiagojsag@gmail.com>
2019-10-09 17:05:32 +0200

    Fix issue where user auth would not be parsed from the correct field

--- geetiles/middleware.py
+++ geetiles/middleware.py
@@ -1,17 +1,17 @@
 import logging
-import json
-
 from functools import wraps
+
 from flask import request, redirect
 
 from geetiles.errors import LayerNotFound
-from geetiles.services.redis_service import RedisService
-from geetiles.services.layer_service import LayerService
 from geetiles.routes.api import error
+from geetiles.services.layer_service import LayerService
+from geetiles.services.redis_service import RedisService
 
 
 def exist_tile(func):
     """Get geodata"""
+
     @wraps(func)
     def wrapper(*args, **kwargs):
         url = RedisService.get(request.path)
@@ -19,36 +19,42 @@ def exist_tile(func):
             return func(*args, **kwargs)
         else:
             return redirect(url)
+
     return wrapper
 
 
 def exist_mapid(func):
     """Get geodata"""
+
     @wraps(func)
     def wrapper(*args, **kwargs):
         layer = kwargs['layer']
         logging.debug('Checking if exist in cache layer ' + layer)
         kwargs["map_object"] = RedisService.check_layer_mapid(layer)
         return func(*args, **kwargs)
+
     return wrapper
 
 
 def is_microservice_or_admin(func):
     """Get geodata"""
+
     @wraps(func)
     def wrapper(*args, **kwargs):
         logging.debug("Checking microservice user")
-        logged_user = json.loads(request.args.get("loggedUser", None))
-        if (logged_user.get("id") == "microservice") or (logged_user.get("role") == "ADMIN") :
-            logging.debug("is microservice or admin");
+        logged_user = request.json.get("loggedUser", None)
+        if (logged_user.get("id") == "microservice") or (logged_user.get("role") == "ADMIN"):
+            logging.debug("is microservice or admin")
             return func(*args, **kwargs)
         else:
             return error(status=403, detail="Not authorized")
+
     return wrapper
 
 
 def get_layer(func):
     """Get geodata"""
+
     @wraps(func)
     def wrapper(*args, **kwargs):
         try:
@@ -61,4 +67,5 @@ def get_layer(func):
             return error(status=404, detail=e.message)
         except Exception as e:
             return error(detail='Generic error')
+
     return wrapper
